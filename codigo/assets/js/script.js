aab368565b43d2dba23c45030104a25b1b8d1a4c
Vgoddard <vinicius.goddard@gmail.com>
2023-05-11 05:20:20 -0300

    + zonas diagonais + pagina config

--- codigo/assets/js/script.js
+++ codigo/assets/js/script.js
@@ -2,7 +2,73 @@ var isMenuHidden = 0;
 var viewportHeight = $(window).height();
 var viewportWidth = $(window).width();
 
+
+
 //cores
+
+// ----------- funções -----------
+
+// config-page
+function showConfigPage () {
+    isConfigPageOn = 1;
+    console.log(isConfigPageOn);
+    const pageStart = document.querySelector('#config-page-wrapper');
+
+    setTimeout(function(){
+        $('#config-page').css({
+            opacity: '1',
+        })
+    },1);
+
+    pageStart.innerHTML += `
+    <div id="config-page">
+        <section id="config-page-content"><section>
+    </div>`; 
+
+    const configPageContent = document.querySelector('#config-page-content');
+    configPageContent.innerHTML += `
+    <header>
+        <div id="config-page-leave">
+            <img id="a" src="./assets/img/cruz.png" alt="Sair">
+        </div>
+        <p>OLÁ USUÁRIO</p>
+        
+    </header>
+    
+     <ul>
+         <li id="tema">a</li>
+         <li>a</li>
+         <li>a</li>
+         <li>a</li>
+         <li>a</li>
+         <li>a</li>
+     </ul>
+    `;
+
+    setTimeout(function(){
+       $('#config-page-content').css({
+            opacity: '1',
+        });
+    },1000);
+};
+function hideConfigPage () {
+    isConfigPageOn = 0;
+    console.log(isConfigPageOn);
+    const pageEnd = document.querySelector('#config-page-wrapper');
+
+    $('#config-page-content').css({
+        opacity: '0',
+    });
+
+    setTimeout(function(){
+        $('#config-page').css({
+            opacity: '0',
+        });
+        setTimeout(function(){
+            pageEnd.innerHTML = ``; 
+        },3000);
+    },1000); 
+}
 
 
 

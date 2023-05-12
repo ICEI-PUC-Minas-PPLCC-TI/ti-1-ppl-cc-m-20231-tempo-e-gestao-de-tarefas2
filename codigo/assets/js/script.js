b78a7a8fce7a2cf6e1031c314700798c3ec8cec5
Thomas Neuenschwander <tbaron@sga.pucminas.br>
2023-05-11 22:33:31 -0300

    nao sei oq eu mudei kkkk

--- codigo/assets/js/script.js
+++ codigo/assets/js/script.js
@@ -6,5 +6,26 @@ var viewportWidth = $(window).width();
 var cor1, cor2, cor3, cor4, cor5;
 
 
+//CODIGO TEXTO Q DIGITA SOZINHO
+
+const el = document.querySelector('#text');
+const text = "1 melhor cada dia!";
+const interval = 200;
+
+function showText(el,text,interval){
+    const char = text.split("").reverse();
+    const typer = setInterval(() => {
+
+        if(!char.length){
+            return clearInterval(typer);
+        }
+        const next = char.pop();
+
+        el.innerHTML += next;
+
+    },interval);
+}
+
+showText(el,text,interval);
 
 

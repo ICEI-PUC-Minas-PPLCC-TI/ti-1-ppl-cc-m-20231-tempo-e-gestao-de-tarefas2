a262d69a8f778f7e7a37758d28965faa0d53aca2
Thomas Neuenschwander <tbaron@sga.pucminas.br>
2023-06-10 10:32:22 -0300

    CSS Informante Digital feito

--- codigo/assets/js/infoD.js
+++ codigo/assets/js/infoD.js
@@ -136,7 +136,7 @@ setInterval(function () {
   const infContainer = document.querySelector('.inf-conjunto-container');
 
   function roger() {
-    $('.inf-conjunto-container').css("background-color", "#333333");
+    $('.inf-conjunto-container').css("background-color", "#fff");
     infContainer.innerHTML = ` 
       <section class="quadrado">
       <section class="paraesq">
@@ -146,10 +146,10 @@ setInterval(function () {
       </section>
 
       <ul>
-      <li><strong>Sono Ideal:</strong> Um adulto deve dormir entre 7-9 horas e passar por todos os ciclos do sono.</li>
-      <li><strong>Benefícios:</strong> Um sono de qualidade limpa a adenosina do cérebro, molécula responsável pela fadiga.</li>
-      <li><strong>Desempenho cognitivo:</strong> O sono adequado melhora a memória, a criatividade e o raciocínio, maximizando o desempenho intelectual.</li>
-      <li>É durante o sono que ocorre a <strong>neuroplaticidade</strong>, processo fisiológico em que os neuronios conseguem criar novas conexoes sinápticas.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <strong>Sono Ideal:</strong> Um adulto deve dormir entre 7-9 horas e passar por todos os ciclos do sono.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <strong>Benefícios:</strong> Um sono de qualidade limpa a adenosina do cérebro, molécula responsável pela fadiga.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <strong>Desempenho cognitivo:</strong> O sono adequado melhora a memória, a criatividade e o raciocínio, maximizando o desempenho intelectual.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> É durante o sono que ocorre a <strong>neuroplaticidade</strong>, processo fisiológico em que os neuronios conseguem criar novas conexoes sinápticas.</li>
     </ul>
 
       </section>
@@ -224,9 +224,9 @@ setInterval(function () {
       </section>
 
       <ul>
-      <li><strong>Córtex pré-frontal:</strong> Região importante para a manutenao da atencao.        <a href="https://www.youtube.com/watch?v=G7WLXoPWQjs" target="_blank">https://www.youtube.com/watch?v=G7WLXoPWQjs</a></li>
-      <li>Devido a liberacao de <a href="https://www.youtube.com/watch?v=plfnROQzBXY" target="_blank"><strong>dopamina</strong></a> maneira mais fácil e eficiente para <strong>conseguir prestar atencao</strong> em algo é estando <strong>motivado</strong>/gostando de executar este comportamento.</li>
-      <li>O <a href="https://www.youtube.com/watch?v=G7WLXoPWQjs" target="_blank"><strong>Pomodoro</strong></a>, ferramenta imbutida no própio Chronos, é uma <strong>técnica de gestão de tempo</strong> que envolve trabalhar em intervalos de tempo focados, seguidos por pausas curtas. Sendo uma excelente técnica para aumentar o foco.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <strong>Córtex pré-frontal:</strong> Região importante para a manutenao da atencao.        <a href="https://www.youtube.com/watch?v=G7WLXoPWQjs" target="_blank">https://www.youtube.com/watch?v=G7WLXoPWQjs</a></li>
+      <li><i class="fa-solid fa-arrow-right"></i> Devido a liberacao de <a href="https://www.youtube.com/watch?v=plfnROQzBXY" target="_blank"><strong>dopamina</strong></a> maneira mais fácil e eficiente para <strong>conseguir prestar atencao</strong> em algo é estando <strong>motivado</strong>/gostando de executar este comportamento.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> O <a href="https://www.youtube.com/watch?v=G7WLXoPWQjs" target="_blank"><strong>Pomodoro</strong></a>, ferramenta imbutida no própio Chronos, é uma <strong>técnica de gestão de tempo</strong> que envolve trabalhar em intervalos de tempo focados, seguidos por pausas curtas. Sendo uma excelente técnica para aumentar o foco.</li>
     </ul>
 
       </section>
@@ -260,10 +260,10 @@ setInterval(function () {
       </section>
 
       <ul>
-      <li>Um adulto <strong>deve beber de água</strong> entre <strong>35 a 65 ml por kg corporal</strong>.</li>
-      <li>Pessoas <strong>desidratadas</strong> apresentam menor volume de sangue que o normal, o que acaba atrapalhando o funcionamento do <strong>cérebro</strong> (cognição) e <strong>coração</strong>.</li>
-      <li>Cerca de <strong>75% do peso de um músculo é composto por água</strong>. Quem busca o ganho de massa magra nao pode pecar na hidratacao.</li>
-      <li><a href="https://www.youtube.com/watch?v=qgscCIhugNA" target="_blank">https://www.youtube.com/watch?v=qgscCIhugNA</a></li>
+      <li><i class="fa-solid fa-arrow-right"></i> Um adulto <strong>deve beber de água</strong> entre <strong>35 a 65 ml por kg corporal</strong>.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> Pessoas <strong>desidratadas</strong> apresentam menor volume de sangue que o normal, o que acaba atrapalhando o funcionamento do <strong>cérebro</strong> (cognição) e <strong>coração</strong>.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> Cerca de <strong>75% do peso de um músculo é composto por água</strong>. Quem busca o ganho de massa magra nao pode pecar na hidratacao.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <a href="https://www.youtube.com/watch?v=qgscCIhugNA" target="_blank">https://www.youtube.com/watch?v=qgscCIhugNA</a></li>
     </ul>
       </section>
 
@@ -296,10 +296,10 @@ setInterval(function () {
       </section>
 
       <ul>
-      <li>O vício é uma <strong>dependência</strong> física e/ou psicológica de substâncias ou comportamentos, com alterações nos <strong>circuitos de recompensa do cérebro</strong>.</li>
-      <li>Um vício geralmente está atrelado a um <strong>gatilho</strong> (pessoa, ambiente, lugares e épocas).</li>\
-      <li>Para perder um hábito ruim é preciso <strong>mudar de ambiente</strong> (É no ambiente onde estao os gatilhos).</li>
-      <li><strong><a href="https://www.youtube.com/watch?v=MMvc3esvCRI" target="_blank">Como trocar vícios RUINS por vícios SAUDÁVEIS</a></strong></li>
+      <li><i class="fa-solid fa-arrow-right"></i> O vício é uma <strong>dependência</strong> física e/ou psicológica de substâncias ou comportamentos, com alterações nos <strong>circuitos de recompensa do cérebro</strong>.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> Um vício geralmente está atrelado a um <strong>gatilho</strong> (pessoa, ambiente, lugares e épocas).</li>\
+      <li><i class="fa-solid fa-arrow-right"></i> Para perder um hábito ruim é preciso <strong>mudar de ambiente</strong> (É no ambiente onde estao os gatilhos).</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <strong><a href="https://www.youtube.com/watch?v=MMvc3esvCRI" target="_blank">Como trocar vícios RUINS por vícios SAUDÁVEIS</a></strong></li>
     </ul>
 
       </section>
@@ -334,9 +334,9 @@ setInterval(function () {
       </section>
 
       <ul>
-      <li><strong>O que é</strong> TDAH?: O Transtorno de Défit de Atencao e hiperatividade é uma doença <strong>crônica</strong> que inclui <strong>dificuldade de atenção, hiperatividade e impulsividade</strong>.</li>
-      <li>O <strong><a href="https://www.youtube.com/watch?v=HMW0oOXA2ww" target="_blank">TDAH</a></strong> é um transtorno que atinge cerca de 3 a 5% da populacao mundial.</li>
-      <li>A <strong><a href="https://www.youtube.com/watch?v=ZnDHYQ6kKNo" target="_blank">depressao</a></strong> e a <strong><a href="https://www.youtube.com/watch?v=OAT1imQ05bU" target="_blank">ansiedade</a></strong> sao transtornos mentais comuns que afetam drásticamente a qualidade de vida e a <strong>produtividade</strong> do individuo.</li>
+      <li> <i class="fa-solid fa-arrow-right"></i> <strong>O que é</strong> TDAH?: O Transtorno de Défit de Atencao e hiperatividade é uma doença <strong>crônica</strong> que inclui <strong>dificuldade de atenção, hiperatividade e impulsividade</strong>.</li>
+      <li> <i class="fa-solid fa-arrow-right"></i> O <strong><a href="https://www.youtube.com/watch?v=HMW0oOXA2ww" target="_blank">TDAH</a></strong> é um transtorno que atinge cerca de 3 a 5% da populacao mundial.</li>
+      <li> <i class="fa-solid fa-arrow-right"></i> A <strong><a href="https://www.youtube.com/watch?v=ZnDHYQ6kKNo" target="_blank">depressao</a></strong> e a <strong><a href="https://www.youtube.com/watch?v=OAT1imQ05bU" target="_blank">ansiedade</a></strong> sao transtornos mentais comuns que afetam drásticamente a qualidade de vida e a <strong>produtividade</strong> do individuo.</li>
     </ul>
 
     <!--
@@ -378,10 +378,10 @@ setInterval(function () {
       </section>
 
       <ul>
-      <li>Atividades físicas melhoram o <strong>desempenho cognitivo, qualidade de sono, condicionamento muscular e reduzem o estresse</strong> (além de aumentar a sencacao de bem-estar).</li>
-      <li>Práticar exercícios físicos tambem podem se tornar um vício, basta condicionar a adequadamente a recompensa desse comportamento.</li>
-      <li><a href="https://www.youtube.com/watch?v=sjH9jBd68yE" target="_blank">https://www.youtube.com/watch?v=sjH9jBd68yE</a></li>
-      <li>Confira a <strong>playlist</strong> de <strong>TREINO</strong> da Chronos: <a href="https://open.spotify.com/playlist/0TjWhwCALXzTHu6Guqg1BR?si=8d2f9a6be1e74c66" target="_blank">https://open.spotify.com/playlist/0TjWhwCALXzTHu6Guqg1BR?si=8d2f9a6be1e74c66</a></li>
+      <li><i class="fa-solid fa-arrow-right"></i> Atividades físicas melhoram o <strong>desempenho cognitivo, qualidade de sono, condicionamento muscular e reduzem o estresse</strong> (além de aumentar a sencacao de bem-estar).</li>
+      <li><i class="fa-solid fa-arrow-right"></i> Práticar exercícios físicos tambem podem se tornar um vício, basta condicionar a adequadamente a recompensa desse comportamento.</li>
+      <li><i class="fa-solid fa-arrow-right"></i> <a href="https://www.youtube.com/watch?v=sjH9jBd68yE" target="_blank">https://www.youtube.com/watch?v=sjH9jBd68yE</a></li>
+      <li><i class="fa-solid fa-arrow-right"></i> Confira a <strong>playlist</strong> de <strong>TREINO</strong> da Chronos: <a href="https://open.spotify.com/playlist/0TjWhwCALXzTHu6Guqg1BR?si=8d2f9a6be1e74c66" target="_blank">https://open.spotify.com/playlist/0TjWhwCALXzTHu6Guqg1BR?si=8d2f9a6be1e74c66</a></li>
     </ul>
 
       </section>
